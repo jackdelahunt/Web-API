2ee3520f190a81c3339ba61e1ecf04387c9f6eb5
jackdelahunt <20085409@mail.wit.ie>
2020-11-18 11:53:32 +0000

    node-lab1,  greeting module added

--- node-lab1/index.js
+++ node-lab1/index.js
@@ -1,15 +1,25 @@
 import http from "http";
 import dotenv from "dotenv";
+import greeting from "./greeting.js";
 
 dotenv.config();
 
 const port = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  response.writeHead(200);
-  response.end("Hello WAD2");
+  let lang = req.headers["accept-language"];
+  const defaultLang="en";
+  if (!greeting[lang]) lang=defaultLang;
+  const response={
+    lang: lang,
+    message: greeting[lang],
+  };
+
+  res.writeHead(200, {"Content-Type": "text/plain",
+    "Content-Language": response.lang});
+  res.end(response.message);
 });
 
 server.listen(port);
 
-console.log("RUNNING");
+console.log(port);
