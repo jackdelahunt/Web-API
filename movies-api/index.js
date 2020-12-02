d46557720d5caf36d9ba1f7f6cd500ca6db3d31b
Jack Delahunt <20085409@mail.wit.ie>
2020-12-02 14:22:01 +0000

    Week 9 Exercises

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 import './db';
 import {loadUsers} from './seedData'
 import usersRouter from './api/users'
+import genreRouter from './api/genres'
 
 dotenv.config();
 
@@ -29,6 +30,7 @@ app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/genres', genreRouter);
 app.use(errHandler);
 
 app.listen(port, () => {
